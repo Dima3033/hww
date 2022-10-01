237cd27ce7814ddac808fd7fb11a2e1790370bb9
Dima <livitorskripunovich@gmail.com>
2022-10-01 16:09:29 +0300

    Update: Add 'Завдання 3'

---
+++ HW.py
@@ -0,0 +1,7 @@
+num_1 = int(input('num1->'))
+if num_1 > 0:
+    print("Number is positive")
+if num_1 < 0:
+        print("Number is negative")
+if num_1 == 0:
+    print("Number is equal to zeero")
